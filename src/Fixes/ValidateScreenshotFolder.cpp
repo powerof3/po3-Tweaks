49fd4e3da2a6dce47ed794d5c72783e04ffd3c4b
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-03-06 01:27:59 +0530

    fix crash

--- src/Fixes/ValidateScreenshotFolder.cpp
+++ src/Fixes/ValidateScreenshotFolder.cpp
@@ -6,6 +6,13 @@
 
 namespace Fixes::ValidateScreenshotFolder
 {
+	RE::Setting* set_ini_string(RE::Setting* a_setting, const char* a_str)
+	{
+		using func_t = decltype(&set_ini_string);
+		REL::Relocation<func_t> func{ RELOCATION_ID(73882, 75619) };
+		return func(a_setting,a_str);
+	}
+	
 	bool has_root_directory(const std::filesystem::path& a_path)
 	{
 		auto path = a_path.string();
@@ -21,63 +28,50 @@ namespace Fixes::ValidateScreenshotFolder
 	void Install()
 	{
 		if (auto setting = RE::GetINISetting("sScreenShotBaseName:Display")) {
-			try {
-				std::filesystem::path gameDirectory = std::filesystem::current_path();
-				gameDirectory.make_preferred();
+			std::filesystem::path gameDirectory = std::filesystem::current_path();
+			gameDirectory.make_preferred();
 
-				std::filesystem::path screenshotFolder{};
-				std::string           newBaseName{};
+			std::filesystem::path screenshotFolder{};
+			std::string           newBaseName{};
 
-				auto folder = setting->GetString();
-				bool emptyPath = string::is_empty(folder);
+			auto folder = setting->GetString();
+			bool emptyPath = string::is_empty(folder);
 
+			if (emptyPath) {
+				screenshotFolder = gameDirectory;
+				newBaseName = "Screenshot";
+			} else {
+				screenshotFolder = folder;
+				screenshotFolder.make_preferred();
+
+				if (has_root_directory(screenshotFolder)) {
+					if (!is_subpath(screenshotFolder, gameDirectory) && !std::filesystem::exists(screenshotFolder)) {
+						newBaseName = "Screenshot";
+					}
+				} else {
+					// folder will be generated in skyrim root
+					screenshotFolder = gameDirectory /= screenshotFolder;
+					screenshotFolder.remove_filename();
+				}
+			}
+
+			if (!newBaseName.empty()) {
+				set_ini_string(setting, newBaseName.c_str());
+				
 				if (emptyPath) {
-					screenshotFolder = gameDirectory;
-					newBaseName = "Screenshot";
+					RE::ConsoleLog::GetSingleton()->Print("[po3 Tweaks] sScreenShotBaseName:Display ini setting is empty");
 				} else {
-					screenshotFolder = folder;
-					screenshotFolder.make_preferred();
-
-					if (has_root_directory(screenshotFolder)) {
-						if (!is_subpath(screenshotFolder, gameDirectory) && !std::filesystem::exists(screenshotFolder)) {
-							newBaseName = "Screenshot";
-						}
-					} else {
-						// folder will be generated in skyrim root
-						screenshotFolder = gameDirectory /= screenshotFolder;
-						screenshotFolder.remove_filename();
-					}
+					RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Screenshot folder ({}) does not exist", screenshotFolder.string()).c_str());
 				}
-
-				if (!newBaseName.empty()) {
-					//to-do: port this to clib
-					if (setting->data.s) {
-						RE::free(setting->data.s);
-					}
-
-					std::size_t strLen = newBaseName.length() + 1;
-					setting->data.s = RE::malloc<char>(strLen);
-					std::memcpy(setting->data.s, newBaseName.c_str(), sizeof(char) * strLen);
-
-					if (emptyPath) {
-						RE::ConsoleLog::GetSingleton()->Print("[po3 Tweaks] sScreenShotBaseName:Display ini setting is empty");
-					} else {
-						RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Screenshot folder ({}) does not exist", screenshotFolder.string()).c_str());
-					}
-					RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Defaulting to {} folder\n", gameDirectory.string()).c_str());
-					if (emptyPath) {
-						logger::info("\t\tValidated screenshot location ({})"sv, screenshotFolder.string());
-					} else {
-						logger::info("\t\tValidated screenshot location ({} -> {})"sv, screenshotFolder.string(), gameDirectory.string());
-					}
-				} else {
-					RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Screenshot folder ({}) validated successfully\n", screenshotFolder.string()).c_str());
+				RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Defaulting to {} folder\n", gameDirectory.string()).c_str());
+				if (emptyPath) {
 					logger::info("\t\tValidated screenshot location ({})"sv, screenshotFolder.string());
+				} else {
+					logger::info("\t\tValidated screenshot location ({} -> {})"sv, screenshotFolder.string(), gameDirectory.string());
 				}
-			} catch (std::exception& e) {
-				logger::info("\t\tUnable to validate screenshot path (error: {})"sv, e.what());
-			} catch (...) {
-				logger::info("\t\tUnable to validate screenshot path (unknown error)"sv);
+			} else {
+				RE::ConsoleLog::GetSingleton()->Print(std::format("[po3 Tweaks] Screenshot folder ({}) validated successfully\n", screenshotFolder.string()).c_str());
+				logger::info("\t\tValidated screenshot location ({})"sv, screenshotFolder.string());
 			}
 		}
 	}
