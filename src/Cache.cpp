9bff4d75cc7735c9fceee596b771f8f65e983008
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-06-08 19:49:51 +0530

    Standardize hook

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -5,7 +5,7 @@ namespace Cache
 	void EditorID::CacheEditorID(const RE::TESForm* a_form, const char* a_editorID)
 	{
 		Locker locker(_lock);
-		_formIDToEditorIDMap.emplace(a_form->GetFormID(), a_editorID);
+		_formIDToEditorIDMap.try_emplace(a_form->GetFormID(), a_editorID);
 	}
 
 	const std::string& EditorID::GetEditorID(RE::FormID a_formID)
--- src/Fixes/FirstPersonAlpha.cpp
+++ src/Fixes/FirstPersonAlpha.cpp
@@ -5,33 +5,24 @@ namespace Fixes::FirstPersonAlpha
 {
 	struct FirstPersonAlpha
 	{
-		static void Install()
-		{
-			REL::Relocation<std::uintptr_t> target{ REL_ID(37777, 38722), 0x55 };
-
-			auto& trampoline = SKSE::GetTrampoline();
-			SKSE::AllocTrampoline(14);
-
-			_SetFPAlpha = trampoline.write_call<6>(target.address(), SetFPAlpha);
-		}
-
-	private:
-		static RE::NiAVObject* SetFPAlpha(RE::PlayerCharacter* player, float alpha_value)
+		static RE::NiAVObject* thunk(RE::PlayerCharacter* a_player, float a_alpha)
 		{
 			// fade == false -> alpha_value = 2 to 3
 			// fade == true -> alpha_value = 0 to 1
-			if (alpha_value >= 2) {
-				alpha_value -= 2;
+			if (a_alpha >= 2) {
+				a_alpha -= 2;
 			}
-			player->Get3D(true)->UpdateMaterialAlpha(alpha_value, false);
+			a_player->Get3D(true)->UpdateMaterialAlpha(a_alpha, false);
 			return nullptr;  // Return null so that the original fade function for 1st person doesn't execute
 		}
-		static inline REL::Relocation<decltype(SetFPAlpha)> _SetFPAlpha;
+		static inline REL::Relocation<decltype(thunk)> func;
 	};
 
 	void Install()
 	{
-		FirstPersonAlpha::Install();
+		REL::Relocation<std::uintptr_t> target{ REL_ID(37777, 38722), 0x55 };
+		stl::write_thunk_call<FirstPersonAlpha, 6>(target.address());
+		
 		logger::info("\t\tInstalled first person alpha fix"sv);
 	}
 }
