30303eb0f766e1782caee7be40315698cf5ef610
powerof3 <32599957+powerof3@users.noreply.github.com>
2026-08-24 20:44:37 +0000

    maintenance

--- src/main.cpp
+++ src/main.cpp
@@ -96,7 +96,7 @@ SKSE_PLUGIN_LOAD(const SKSE::LoadInterface* a_skse)
 						   .trampolineSize = 450 });
 
 	const auto runtimeVersion = a_skse->RuntimeVersion();
-	
+
 	REX::INFO("Game version : {}", runtimeVersion);
 
 	if constexpr (SKSE::RUNTIME_SSE_LATEST < MIN_ADDRESS_LIBRARY_V5_RUNTIME) {
